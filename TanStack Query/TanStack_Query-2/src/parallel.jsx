146efec3d459e187182ea87fb73c60764cdeaee2
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-06-25 14:04:31 +0530

    Refactor Parallel component to use useQueries hook
    This commit refactors the Parallel component to use the `useQueries` hook from TanStack Query. This allows for fetching multiple users in parallel, improving performance. The component now maps over an array of user IDs and creates a query for each one.

--- TanStack Query/TanStack_Query-2/src/parallel.jsx
+++ TanStack Query/TanStack_Query-2/src/parallel.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import { useQueries } from "@tanstack/react-query";
+import { useState } from "react";
 
 const Parallel = () => {
-    const [userIds, setUserIds] = React.useState([1]);
+  const [userIds, setUserIds] = useState([1, 2]);
 
-    return (
-        <div>
-            <button
-                onClick={() =>
-                    setUserIds((prev) => {
-                        return [...prev, Date.now()];
-                    })
-                }>
-                Load more
-            </button>
+  //! Error
+  //   userIds.forEach((id) => {
+  //     const useQuery = useQuery({
+  //       queryKey: ["user", id],
+  //       queryFn: async () => {
+  //         const data = await fetch(`https://dummyjson.com/users/${id}`).then(
+  //           (res) => res.json()
+  //         );
+  //         return data;
+  //       },
+  //     });
+  //   });
 
-            {userIds.map((id) => (
-                <h1 key={id}>{id}</h1>
-            ))}
-        </div>
-    );
+  const userQueries = useQueries({
+    queries: userIds.map((id) => {
+      return {
+        queryKey: ["user", id],
+        queryFn: async () => {
+          const data = await fetch(`https://dummyjson.com/users/${id}`).then(
+            (res) => res.json()
+          );
+          return data;
+        },
+      };
+    }),
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          setUserIds((prev) => {
+            return [...prev, Date.now()];
+          })
+        }
+      >
+        Load more
+      </button>
+
+      {userIds.map((id) => (
+        <h1 key={id}>{id}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default Parallel;
