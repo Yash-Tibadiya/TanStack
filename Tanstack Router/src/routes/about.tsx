57bf9414c3aab9ce901484033343b73463e4e6d8
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-06-25 18:47:49 +0530

    about us

--- Tanstack Router/src/routes/about.tsx
+++ Tanstack Router/src/routes/about.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  return (
+    <div className="p-10">
+      <h2 className="text-4xl font-bold mb-2">About Us</h2>
+      <p className="text-gray-800 mb-3">This is about us</p>
+    </div>
+  );
 }
